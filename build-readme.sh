12442eb84d64bb38b1e5a09c192a48b3c72a9b40
Cory Collier <cory.collier@clearwatercompliance.com>
2020-11-29 11:10:28 -0500

    updating how the build-readme script reports details

--- build-readme.sh
+++ build-readme.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
+# Setup critical base information
 project="$(basename "$(realpath .)" | sed -e 's/docker-//')"
 tag="$(git branch --show-current)"
 
-echo "building mck7/${project}:${tag}"
-
+# build the image, run it as "build-readme"
 docker build --rm -t "mck7/${project}:${tag}" .
-
-
 docker rm -f build-readme
 docker run -itd --name build-readme "mck7/${project}:${tag}"
-phpm="$(docker exec -it build-readme bash -c "php -m")"
+
+# Collect information
+php_module="$(docker exec -it build-readme bash -c "php -m")"
+php_version="$(docker exec -it build-readme bash -c "php -v")"
 headings="$(head -n2 README.md)"
 
+# Begin the README output
 echo "${headings}" > README.md
+
+# Send the PHP version information
+echo "
+## PHP Version Information
+" >> README.md
 echo '```' >> README.md
-echo "${phpm}" >> README.md
+echo "${php_version}" >> README.md
+echo '```' >> README.md
+
+# Send the php module information
+echo "
+## PHP Module Information
+" >> README.md
+echo '```' >> README.md
+echo "${php_module}" >> README.md
 echo '```' >> README.md
